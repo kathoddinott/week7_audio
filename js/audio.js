1e928f5f0e354d76c310f2caaadb420fc9f7e522
Hoddinott <k_hoddinott2@tss-hr420-dm13.local>
2019-02-25 15:45:36 -0500

    second commit

--- js/audio.js
+++ js/audio.js
@@ -1,9 +1,10 @@
-{() => {
+(() => {
 
 	let aud = document.querySelector('audio'),
 		play = document.querySelector('playButton'),
-		pause = document.querySelecto('pauseButton'),
-		rewind = document.querySelctor('rewindButton');
+		pause = document.querySelector('pauseButton'),
+		rewind = document.querySelector('rewindButton'),
+		tracks = document.querySelectorAll('.trackholder');
 
 	function playAudio() {
 		aud.play();
@@ -17,6 +18,18 @@
 		aud.currentTime = 0;
 	}
 
+	function swapSources() {
+		let currentTrack = this.dataset.currenttrack;
+
+		aud.src = `audio/${currentTrack}`;
+		aud.load();
+		aud.play();
+	}
+
 	play.addEventListener("click", playAudio);
 	pause.addEventListener("click", pauseAudio);
 	rewind.addEventListener("click", rewindAudio);
+
+	tracks.forEach(track => track.addEventListener("click", swapSource));
+
+})();
